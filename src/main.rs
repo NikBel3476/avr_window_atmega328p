038d263d7416c1e7e59e239dd70a4dc2b5d66bdd
NikBel3476 <nikita.belkin3476@gmail.com>
2023-06-03 11:32:07 +0400

    chore: remove unused comments

--- src/main.rs
+++ src/main.rs
@@ -22,34 +22,6 @@ use ebyte_e32::{
 use embedded_hal::digital::v2::InputPin;
 use nb::block;
 use panic_halt as _;
-// use ruduino::cores::current;
-// use ruduino::cores::current::{port, DDRF, PORTF};
-// use ruduino::interrupt::without_interrupts;
-// use ruduino::legacy::serial;
-// use ruduino::modules::{ClockSource16, Timer16, WaveformGenerationMode16};
-// use ruduino::{Pin, Register};
-
-// uart
-// const BAUD: u32 = 9600;
-// const UBRR: u16 = (ruduino::config::CPU_FREQUENCY_HZ / 16 / BAUD - 1) as u16;
-
-// timer
-// const DESIRED_HZ_TIM1: f64 = 1.0;
-// const TIM1_PRESCALER: u64 = 1024;
-// const INTERRUPT_EVERY_1_HZ_1024_PRESCALER: u16 =
-// 	((ruduino::config::CPU_FREQUENCY_HZ as f64 / (DESIRED_HZ_TIM1 * TIM1_PRESCALER as f64)) as u64
-// 		- 1) as u16;
-
-// enum WINDOW_STATE {
-// 	Open,
-// 	Close,
-// 	Moving
-// }
-
-// struct GlobalState {
-// 	time: u32,
-// 	serial0: *mut Usart<Atmega, USART0, Pin<Input, PE0>, Pin<Output, PE1>, MHz16>
-// }
 
 const MESSAGE_SEPARATOR: u8 = b';';
 const SECONDS_IN_DAY: u32 = 24 * 60 * 60;
@@ -104,7 +76,6 @@ static mut WINDOW_IS_OPENED: AtomicBool = AtomicBool::new(false);
 static mut WINDOW_IS_CLOSED: AtomicBool = AtomicBool::new(true);
 static mut WINDOW_IS_OPENING: AtomicBool = AtomicBool::new(false);
 static mut WINDOW_IS_CLOSING: AtomicBool = AtomicBool::new(false);
-// static mut LED: mem::MaybeUninit<Pin<Output, port::PB5>> = mem::MaybeUninit::uninit();
 static mut GLOBAL_TIME_IN_SEC: u32 = 0;
 static mut TIME_MODE: TimeMode = TimeMode {
 	active_time_in_sec: 0,
@@ -123,21 +94,6 @@ static mut SCHEDULE: Schedule = Schedule {
 
 #[arduino_hal::entry]
 fn main() -> ! {
-	// without_interrupts(|| {
-	// 	current::Timer16::setup()
-	// 		.waveform_generation_mode(WaveformGenerationMode16::ClearOnTimerMatchOutputCompare)
-	// 		.clock_source(ClockSource16::Prescale1024)
-	// 		.output_compare_1(Some(INTERRUPT_EVERY_1_HZ_1024_PRESCALER))
-	// 		.configure();
-
-	// 	serial::Serial::new(UBRR)
-	// 		.character_size(serial::CharacterSize::EightBits)
-	// 		.mode(serial::Mode::Asynchronous)
-	// 		.parity(serial::Parity::Disabled)
-	// 		.stop_bits(serial::StopBits::OneBit)
-	// 		.configure();
-	// });
-
 	let mut receiving_message: [u8; 50] = [0; 50];
 	let mut receiving_message_position = 0;
 	let mut message_received = false;
@@ -153,67 +109,7 @@ fn main() -> ! {
 		BaudrateExt::into_baudrate(9600),
 	);
 
-	// unsafe {
-	// 	// SAFETY: Interrupts are not enabled at this point so we can safely write the global
-	// 	// variable here.  A memory barrier afterwards ensures the compiler won't reorder this
-	// 	// after any operation that enables interrupts.
-	// 	INTERRUPT_STATE = mem::MaybeUninit::new(InterruptState {
-	// 			time: 0
-	// 	});
-	// 	core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
-	// }
-
 	let timer1 = dp.TC1;
-
-	// let mut serial2 = arduino_hal::Usart::new(
-	// 	dp.USART1,
-	// 	pins.pd2,
-	// 	pins.pd3.into_output(),
-	// 	BaudrateExt::into_baudrate(9600),
-	// );
-
-	// let m0 = pins.pe2.into_output();
-	// let m1 = pins.pe3.into_output();
-	// let aux = pins.pe4.into_floating_input();
-	// let delay_lora = arduino_hal::Delay::new();
-
-	// let mut ebyte = Ebyte::new(serial1, aux, m0, m1, delay_lora).unwrap_or_else(|_| loop {
-	// 	serial2.write_char('e');
-	// });
-
-	// let mut led = pins.pb5.into_output();
-	// led.set_low();
-	// unsafe {
-	// 	LED = mem::MaybeUninit::new(led);
-	// 	core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
-	// }
-
-	// let mut led = pins.pb0.into_output().downgrade();
-	// led.toggle();
-
-	// port::B5::set_output();
-	// port::B5::set_high();
-	// port::E0::set_output();
-	// port::E0::set_high();
-
-	// pin for engine control(relay)
-	// port::E2::set_output();
-	// port::E2::set_low();
-	// TODO: change pin
-	// let mut relay = pins.pe2.into_output();
-	// relay.set_low();
-
-	// TODO: change pins
-	// let enable_engine_button = pins.pe3.into_floating_input();
-	// let disable_engine_button = pins.pe4.into_floating_input();
-
-	// init leds for lora
-	// unsafe {
-	// 	(*arduino_hal::pac::PORTF::PTR)
-	// 		.ddrf
-	// 		.write(|w| w.bits(0b11111111));
-	// 	(*arduino_hal::pac::PORTF::PTR).portf.write(|w| w.bits(0));
-	// }
 
 	// engine rotation direction
 	// low -> close; high -> open
@@ -236,20 +132,6 @@ fn main() -> ! {
 	}
 
 	loop {
-		// serial::transmit(0b00001111);
-
-		// transmitting data via uart
-		// for &b in b"Hello, from Rust!\n" {
-		// 	serial::transmit(b);
-		// }
-
-		// Read a byte from the serial connection
-		// if let Ok(b) = serial1.read() {
-		// 	unsafe {
-		// 		(*arduino_hal::pac::PORTF::PTR).portf.write(|w| w.bits(b));
-		// 	}
-		// PORTF::write(b);
-
 		if let Ok(byte) = serial1.read() {
 			// serial1.write_byte(byte);
 			if !message_received {
@@ -424,83 +306,6 @@ fn main() -> ! {
 			message_received = false;
 		}
 
-		// delay_ms(1000);
-		// match ebyte.model_data() {
-		// 	Ok(model_data) => {
-		// 		ufmt::uwriteln!(
-		// 			&mut serial2,
-		// 			"{},{},{}\r",
-		// 			model_data.model,
-		// 			model_data.version,
-		// 			model_data.features
-		// 		)
-		// 		.void_unwrap();
-		// 	}
-		// 	Err(_) => {
-		// 		serial2.write_char('e');
-		// 	}
-		// };
-
-		// delay_ms(1000);
-		// match ebyte.parameters() {
-		// 	Ok(params) => {
-		// 		let bytes = params.to_bytes();
-		// 		ufmt::uwriteln!(
-		// 			&mut serial2,
-		// 			"{},{},{},{},{}\r",
-		// 			bytes[0],
-		// 			bytes[1],
-		// 			bytes[2],
-		// 			bytes[3],
-		// 			bytes[4],
-		// 		)
-		// 		.void_unwrap();
-		// 	}
-		// 	Err(_) => {
-		// 		serial2.write_char('e');
-		// 	}
-		// }
-
-		// if let Ok(byte) = serial2.read() {
-		// 	match byte.to_ascii_lowercase() as char {
-		// 		'm' => {
-		// 			let model_data = ebyte.model_data().unwrap_or_else(|e| {
-		// 			match e {
-		// 				ebyte_e32::Error::SerialWrite => {
-		// 					block!(serial2.write('w' as u8)).unwrap();
-		// 				}
-		// 				ebyte_e32::Error::SerialRead => {
-		// 					block!(serial2.write('r' as u8)).unwrap();
-		// 				}
-		// 				ebyte_e32::Error::ReadModelData => {
-		// 					block!(serial2.write('n' as u8)).unwrap();
-		// 				}
-		// 				_ => {
-		// 					block!(serial2.write('e' as u8)).unwrap();
-		// 				}
-		// 			}
-		// 			loop {
-		// 				block!(serial2.write('e' as u8)).unwrap();
-		// 			}
-		// 		});
-
-		// 		block!(serial2.write(model_data.model)).unwrap();
-		// 		block!(serial2.write(model_data.version)).unwrap();
-		// 		block!(serial2.write(model_data.features)).unwrap();
-		// 		},
-		// 		'p' => {},
-		// 		_ => {}
-		// 	}
-		// };
-
-		// if enable_engine_button.is_high() {
-		// 	relay.set_high();
-		// }
-
-		// if disable_engine_button.is_high() {
-		// 	relay.set_low();
-		// }
-
 		unsafe {
 			if TIME_MODE.enabled {
 				match TIME_MODE.action_state {
@@ -544,12 +349,6 @@ fn main() -> ! {
 				}
 			}
 		}
-
-		// read byte if there is something available
-		// if let Some(b) = serial::try_receive() {
-		// 	PORTF::write(0b0);
-		// 	PORTF::set_mask_raw(b);
-		// }
 	}
 }
 
